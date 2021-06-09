c589d7c60b9412c4e2e7eee65726ad1e71a801ea
root <root@ip-172-31-43-246.ap-south-1.compute.internal>
2021-06-09 10:47:39 +0000

    code deploy config v1.0

--- FSE_M2A3_LMS_2020fse012/settings.py
+++ FSE_M2A3_LMS_2020fse012/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'NAME': 'fse',
         'USER': 'postgres',
         'PASSWORD': 'ahad',
-        'HOST': 'ec2-3-88-214-192.compute-1.amazonaws.com',
+        'HOST': 'ec2-34-204-49-234.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
