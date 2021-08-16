50ea9c757421870baf657ddcc239cde9ac87f63e
Ahad+1-at-021295606552 <ahad@tripworld.com>
2021-08-16 17:18:38 +0400

    new api

--- course_management/urls.py
+++ course_management/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('course/<str:id>', CourseAPIView.as_view()),
     path('course/user/<str:email>', CourseUserAPIView.as_view()),
     path('course/<str:courseId>/module', CourseModuleAPIView.as_view()),
-    path('course/<str:courseId>/e-val', EvaluationComponentAPIView.as_view())
+    path('course/<str:courseId>/e-val', EvaluationComponentAPIView.as_view()),
+    path('course/user/<str:email>/test', CourseUserAPIView.as_view()),
 ]
